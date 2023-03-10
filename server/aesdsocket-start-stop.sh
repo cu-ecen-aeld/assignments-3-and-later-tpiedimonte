7a728649c98b06d623e8bc67b5a86d680bb2e114
tpiedimonte <tpiedimonte@gmail.com>
2023-03-10 08:13:49 -0500

    change startup

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -3,11 +3,13 @@
 case "$1" in
     start)
         echo "Starting aesdsocket"
-        start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+        # start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
+        start-stop-daemon --start --exec /usr/bin/aesdsocket -- -d
         ;;
     stop)
         echo "Stopping aesdsocket"
-        start-stop-daemon -K -n aesdsocket
+        # start-stop-daemon -K -n aesdsocket
+        start-stop-daemon --stop --name aesdsocket
         ;;
     *)
         echo "Usage: $0 {start|stop}"
